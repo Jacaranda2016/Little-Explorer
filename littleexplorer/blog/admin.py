bf524983a6020fba03fc55e9172b2d2ddf88a1ef
Hannah Liu <yixiumm@gmail.com>
2017-01-04 13:16:26 +1100

    template update article.html update

--- littleexplorer/blog/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Post1
-admin.site.register(Post1)
--- littleexplorer/home/views.py
+++ littleexplorer/home/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 def index(request):
-    return render(request, 'html/index.html',{})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'index.html', {'posts':posts})
+
+# Create your views here.
+def volton(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'volton/index.html', {'posts':posts})
+
