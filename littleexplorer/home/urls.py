8e5d4c83957b3225e82f27df64df036619154428
Hannah Liu <yixiumm@gmail.com>
2017-01-05 15:10:10 +1100

    update content

--- littleexplorer/home/urls.py
+++ littleexplorer/home/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from .import views
 
 urlpatterns = [
-    url(r'^$',views.index, name= 'index'),
-    url('volton',views.volton, name= 'volton'),
+    url('volton/index',views.volton, name= 'volton'),
+    url('volton/article',views.article, name= 'article'),
+    url('volton/arts',views.articles, name= 'articles'),
 ]
