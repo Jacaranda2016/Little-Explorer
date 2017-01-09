0adf1f703c541e972252cb83a0ad43a847e24ece
Hannah Liu <yixiumm@gmail.com>
2017-01-10 10:56:08 +1100

    content update

--- littleexplorer/home/urls.py
+++ littleexplorer/home/urls.py
@@ -4,5 +4,5 @@ from .import views
 urlpatterns = [
     url('volton/index',views.volton, name= 'volton'),
     url('volton/article',views.article, name= 'article'),
-    url('volton/arts',views.articles, name= 'articles'),
+    url(r'^volton/arts/(\w+)/',views.articles, name= 'articles'),
 ]
