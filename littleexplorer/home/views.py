46fd36ce275bc50d70ce714f3f85c415a0176286
Hannah Liu <yixiumm@gmail.com>
2017-01-10 10:56:19 +1100

    content update

--- littleexplorer/home/views.py
+++ littleexplorer/home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
+from django.core.paginator import *
+from django.template import Context,loader
 
 # Create your views here.
 def index(request):
@@ -16,6 +18,21 @@ def article(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'volton/article.html', {'posts':posts})
 
-def articles(request):
+def articles(request,page=1):
+    after_range_num = 5
+    bevor_range_num = 4
+    if page==None:
+        page=1
+    else:
+        page=int(page)
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'volton/articles.html', {'posts':posts})
+    paginator = Paginator(posts,2)
+    try:
+        posts = paginator.page(page)
+    except(EmptyPage,InvalidPage,PageNotAnInteger):
+        posts = paginator.page(1)
+    if page >= after_range_num:
+        page_range = paginator.page_range[page-after_range_num:page+bevor_range_num]
+    else:
+        page_range = paginator.page_range[0:int(page)+bevor_range_num]
+    return  render(request, 'volton/articles.html', {'posts':posts,'page_range':page_range})
