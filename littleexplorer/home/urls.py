7f22a77accbb50e81c7c1aec68243a32bb5eaf81
Hannah Liu <yixiumm@gmail.com>
2017-01-13 15:47:22 +1100

    update url and article detail

--- littleexplorer/home/urls.py
+++ littleexplorer/home/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from .import views
 
 urlpatterns = [
-    url('volton/index',views.volton, name= 'volton'),
-    url(r'^volton/article/(?P<shortUrl>[-\w]+)/$',views.article, name= 'article'),
-    url(r'^volton/arts/(\w+)/',views.articles, name= 'articles'),
+    url('^$',views.index, name= 'index'),
+    url(r'^article/(?P<shortUrl>[-\w]+)/$',views.article, name= 'article'),
+    url(r'^\arts/(\w+)/',views.articles, name= 'articles'),
 ]
