7e89f4a5579e280d07fd18a33b56ad4762ef0461
Charlie Jiang <yixjiang@deloitte.com.au>
2017-01-13 19:47:11 +1100

    add category

--- littleexplorer/home/models.py
+++ littleexplorer/home/models.py
@@ -7,6 +7,7 @@ from django.template.defaultfilters import slugify
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
+    category = models.ForeignKey('home.Category', null=True)
     title = models.CharField(max_length=200)
     text = RichTextUploadingField()
     thumb = models.CharField(max_length=200, null=True)
@@ -25,6 +26,17 @@ class Post(models.Model):
     def publish(self):
         self.published_date = timezone.now()
         self.save()
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+    thumb = models.CharField(max_length=200, null=True)
+    summary = models.CharField(max_length=1000, null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    text = RichTextUploadingField()
 
     def __str__(self):
         return self.title
